cf677116a49e29d1b07df8677f8e57209447627a
develone <develone@sbcglobal.net>
2015-08-19 17:33:32 -0600

    new build see poky_patch_test.txt image now includes gtkwave iverilog vlc xstools installed. root passwd is raspberrypi and still fits on 4GB SD

--- installing_iverilog/add_gtkwave_iverilog_xstools.sh
+++ installing_iverilog/add_gtkwave_iverilog_xstools.sh
@@ -1,7 +1,9 @@
-#!/bin/bash                                                                     
+#!/bin/bash 
+#copy xstools test files to home
+cp -R xstools_test_files ../                                                                         
 #create an installed package list                                               
 rpm -qa | sort > ~/pi_pkgs.txt                                                  
-                                                                                
+                                                                      
 #copy files needed by XSTOOLS to python location                                
 cp XSBRDINF.xml /usr/lib/python2.7/site-packages                                
 cp py_compile.py /usr/lib/python2.7/site-packages                               
@@ -20,7 +22,7 @@ make install
 #Installing rpms not installed on image created by Yocto                        
 cd  ../wiringpi-extra-rpms/cortexa7hf_vfp_vfpv4_neon/                           
 rpm -Uh wiringpi-dbg-git-r0.cortexa7hf_vfp_vfpv4_neon.rpm                       
-rpm -Uh wiringpi-doc-git-r0.cortexa7hf_vfp_vfpv4_neon.rpm  
+  
 cd ../../gtk-extra-rpms/cortexa7hf_vfp_vfpv4_neon/                    
 rpm -Uh gtk-demo-2.24.28-r0.cortexa7hf_vfp_vfpv4_neon.rpm                           
                        
